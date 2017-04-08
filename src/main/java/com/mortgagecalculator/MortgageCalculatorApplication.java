8eddd986bbb0d553a6bfa27709bc4e45b96e2c03
Cooper <alison@betterment.com>
2017-04-08 12:37:36 -0400

    sample scheduling concept for eventual cron job to read daily rate csv files

--- src/main/java/com/mortgagecalculator/MortgageCalculatorApplication.java
+++ src/main/java/com/mortgagecalculator/MortgageCalculatorApplication.java
@@ -2,8 +2,10 @@ package com.mortgagecalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MortgageCalculatorApplication {
 
     public static void main(String[] args) {
